83d7114a95b6e7cdcbf4bfa1952ba129c0cc1e44
rrmettu <rmettu@tulane.edu>
2024-10-07 19:42:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -61,38 +61,3 @@ C = get_code(T)
 print("Huffman cost:  %d" % huffman_cost(C, f))
 
 
-####### Problem 4 #######
-
-test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant','-elephant'), ('AAAGAATTCA', 'AAA---T-CA')]
-
-def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
-    else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
-
-
-def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
-
-def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
-
-def test_MED():
-    for S, T in test_cases:
-        assert fast_MED(S, T) == MED(S, T)
-                                 
-def test_align():
-    for i in range(len(test_cases)):
-        S, T = test_cases[i]
-        align_S, align_T = fast_align_MED(S, T)
-        assert (align_S == alignments[i][0] and align_T == alignments[i][1])
